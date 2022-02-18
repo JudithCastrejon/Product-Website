9613130ae48848a3217db6c99e7b570e5c32068f
Judith Castrejon <99996270+JudithCastrejon@users.noreply.github.com>
2022-02-18 17:11:10 -0500

    Adding project files via upload

---
+++ CastrejonProject.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    //accordion for book an appointment
+    $(function () {
+        $("#accord").accordion();
+        active:false;
+        collapsible:true;
+
+    });
+
+    //code from w3schools for a slideshow/coursel
+    var slideIndex=0;
+    showSlides();
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+
+
+//Code from wp beaches
+    document.addEventListener("DOMContentLoaded", function(event) {
+
+        /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].onclick = function(){
+                this.classList.toggle("active");
+                this.nextElementSibling.classList.toggle("show");
+            };
+        }
+
+        function setClass(els, className, fnName) {
+            for (var i = 0; i < els.length; i++) {
+                els[i].classList[fnName](className);
+            }
+        }
+
+    });
+
+});
